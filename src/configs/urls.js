986cf4bdadddad30cfbb7060d300ec432e64e5a5
TMysiv <tarasmysiv@gmail.com>
2022-02-08 00:14:56 +0200

    add first part

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -5,5 +5,5 @@ export default baseURL;
 export const urls = {
     discover:'/discover/movie?page=',
     images:'https://image.tmdb.org/t/p/w200/',
-    genres:'discover/movie?with_genres=35&&page='
+    genres:'discover/movie?with_genres=28&sort_by=vote_average.desc&&page='
 }
